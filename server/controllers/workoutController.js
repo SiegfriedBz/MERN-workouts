f6c48203056bf572127021bb687025b5ccb77f32
Siegfried <siegfried.bozza@renuo.ch>
2023-03-08 10:35:21 +0100

    clean up workoutController

--- server/controllers/workoutController.js
+++ server/controllers/workoutController.js
@@ -32,7 +32,7 @@ const createWorkout = async (req, res) => {
     const { title, reps, load } = req.body
     try{
         const newWorkout = await Workout.create({title, reps, load})
-        res.status(200).json(newWorkout)
+        res.status(201).json(newWorkout)
     } catch(error) {
         res.status(400).json({error: error.message})
     }
@@ -53,7 +53,6 @@ const updateWorkout = async (req, res) => {
             return res.status(404).json({error: 'No such workout'})
         }
         return res.status(201).json(workout)
-
     } catch(error) {
         res.status(400).json({error: error.message})
     }
@@ -61,7 +60,6 @@ const updateWorkout = async (req, res) => {
 
 const deleteWorkout = async (req, res) => {
     const { id } = req.params
-    console.log(id)
     if(!mongoose.Types.ObjectId.isValid(id)) {
         return res.status(404).json({error: 'No such workout'})
     }
