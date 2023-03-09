67c4b791b236cc5a0a19e6d76867df4d0517944a
Siegfried <siegfried.bozza@renuo.ch>
2023-03-09 09:15:04 +0100

    refactor workoutRouter

--- server/routes/workoutRouter.js
+++ server/routes/workoutRouter.js
@@ -2,16 +2,14 @@ const express = require('express')
 const router = express.Router()
 const {
     getWorkouts,
-    getWorkout,
     createWorkout,
-    updateWorkout,
+    // updateWorkout,
     deleteWorkout
 } = require('../controllers/workoutController')
 
 router.get('/', getWorkouts)
-router.get('/:id', getWorkout)
 router.post('/', createWorkout)
-router.patch('/:id', updateWorkout)
+// router.patch('/:id', updateWorkout)
 router.delete('/:id', deleteWorkout)
 
 module.exports = router
