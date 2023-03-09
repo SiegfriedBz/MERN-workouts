cd1a211ef7a5f7651eb94da5e0a6441bd036b62b
Siegfried <siegfried.bozza@renuo.ch>
2023-03-09 21:20:05 +0100

    add responsive toggle btn, login, signup, logout

--- client/src/components/Navbar.js
+++ client/src/components/Navbar.js
@@ -1,17 +1,74 @@
 import { Link } from 'react-router-dom'
+import { useAuthContext } from "../hooks/useAuthContext"
+import { useAuthHook } from "../hooks/useAuthHook"
 
 const Navbar = () => {
+    const { user } = useAuthContext()
+    const { logOut } = useAuthHook()
+
     return (
-        <nav className="navbar--wrapper">
-            <ul>
-                <li className="navbar--item">
-                    <Link to='/'aria-current="page">
-                        Workout Buddy
-                    </Link>
-                </li>
-            </ul>
-        </nav>
+        <div className="navbar--wrapper">
+            <span className="navbar--item">
+                <Link to='/'aria-current="page">
+                    Workout Buddy
+                </Link>
+            </span>
+            <div>
+                <nav className="navbar navbar-expand-sm">
+                    <button className="navbar-toggler navbar--btn" type="button" data-bs-toggle="collapse"
+                            data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false"
+                            aria-label="Toggle navigation">
+                        <i className="fa-solid fa-bars"></i>
+                    </button>
+                    <div className="collapse navbar-collapse navbar--sign-container" id="navbarNav">
+                        {user
+                            ? (
+                                <button
+                                    className="logout"
+                                    onClick={logOut}>
+                                    Logout
+                                </button>
+                            )
+                            : (
+                                <>
+                                    <span className="navbar--item">
+                                        <Link to='/login'aria-current="page">
+                                            Login
+                                        </Link>
+                                    </span>
+                                        <span className="navbar--item">
+                                        <Link to='/signup'aria-current="page">
+                                            Signup
+                                        </Link>
+                                    </span>
+                                </>
+                            )
+                        }
+                    </div>
+                </nav>
+            </div>
+        </div>
     )
 }
 
 export default Navbar
+
+            {/*<nav className="navbar--wrapper">*/}
+            {/*        <span className="navbar--item">*/}
+            {/*            <Link to='/'aria-current="page">*/}
+            {/*                Workout Buddy*/}
+            {/*            </Link>*/}
+            {/*        </span>*/}
+            {/*    <div>*/}
+            {/*        <span className="navbar--item">*/}
+            {/*            <Link to='/login'aria-current="page">*/}
+            {/*                Login*/}
+            {/*            </Link>*/}
+            {/*        </span>*/}
+            {/*        <span className="navbar--item">*/}
+            {/*            <Link to='/signup'aria-current="page">*/}
+            {/*                Signup*/}
+            {/*            </Link>*/}
+            {/*        </span>*/}
+            {/*    </div>*/}
+            {/*</nav>*/}
