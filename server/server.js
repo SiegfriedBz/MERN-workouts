8c098b449b47baf1c1ae61a6d8e16203f6662d01
Siegfried <siegfried.bozza@renuo.ch>
2023-03-09 12:32:09 +0100

    add user routes

--- server/server.js
+++ server/server.js
@@ -3,6 +3,7 @@ const express = require('express')
 const mongoose = require('mongoose')
 const cors = require('cors')
 const workoutRouter = require('./routes/workoutRouter')
+const userRouter = require('./routes/userRouter')
 
 // express app
 const app = express()
@@ -17,6 +18,7 @@ app.use((req, res, next) => {
 })
 
 // routes
+app.use('/api/user', userRouter)
 app.use(('/api/workouts'), workoutRouter)
 
 // connect to db & listen to requests
