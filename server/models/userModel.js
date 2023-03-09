9337c7e5befcea4bd68191d9027344703b8efea7
Siegfried <siegfried.bozza@renuo.ch>
2023-03-09 13:19:07 +0100

    add validator logic to userModel

--- server/models/userModel.js
+++ server/models/userModel.js
@@ -1,5 +1,6 @@
 const mongoose = require('mongoose')
 const bcrypt = require('bcrypt')
+const validator = require('validator')
 
 const userSchema = new mongoose.Schema({
     email: {
@@ -14,6 +15,18 @@ const userSchema = new mongoose.Schema({
 })
 
 userSchema.statics.signup = async function(email, password) {
+
+    // validation
+    if(!email || !password) {
+        throw Error('All fields must be filled')
+    }
+    if(!validator.isEmail(email)) {
+        throw Error('Please use a valid email')
+    }
+    if(!validator.isStrongPassword(password)) {
+        throw Error('Please use a strong password')
+    }
+
     const exists = await this.findOne({ email })
     if (exists) {
         throw Error('Email already in use')
