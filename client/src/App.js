ab1b2bcf0b324d63a2617b2c1304123169261bfa
Siegfried <siegfried.bozza@renuo.ch>
2023-03-10 12:35:22 +0100

    add Navigate = f(authContext)

--- client/src/App.js
+++ client/src/App.js
@@ -1,18 +1,20 @@
-import { Routes, Route } from "react-router-dom"
+import { Routes, Route, Navigate } from 'react-router-dom'
+import { useAuthContext } from './hooks/useAuthContext'
 import Navbar from './components/Navbar'
 import Home from './pages/Home'
 import User from './pages/User'
-import UserForm from './components/UserForm'
 
 function App() {
+  const { user } = useAuthContext()
+
   return (
     <div className="App">
         <Navbar />
         <div className="pages">
             <Routes>
-                <Route path='/' element={<Home />} />
-                <Route path='/login' element={<User />} />
-                <Route path='/signup' element={<User />} />
+                <Route path='/' element={user? <Home /> : <Navigate to='/login' />} />
+                <Route path='/login' element={!user ? <User /> : <Navigate to='/' />} />
+                <Route path='/signup' element={!user ? <User /> : <Navigate to='/' />} />
             </Routes>
         </div>
     </div>
