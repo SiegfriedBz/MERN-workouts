e29d2f008f437a2315ed81fb834787b4502a1d91
Siegfried <siegfried.bozza@renuo.ch>
2023-03-10 15:29:54 +0100

    add user_id to workout model

--- server/models/workoutModel.js
+++ server/models/workoutModel.js
@@ -1,6 +1,10 @@
 const mongoose = require('mongoose')
 
 const workoutSchema = new mongoose.Schema({
+    user_id: {
+        type: String,
+        required: true
+    },
     title: {
         type: String,
         required: true
